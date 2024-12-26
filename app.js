2956113da7fc60cae79b420f447479dec99e469c
Puspalal <puspalalnewar79@gmail.com>
2024-12-26 20:08:36 +0530

    Fixed Bug

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const tip = document.querySelector("#tip");
 const btn = document.querySelector(".button");
 const result = document.querySelector(".result");
 
-let a = bill.value;
-let b = tip.value;
-
-// console.log(a * b);
-
 function calcTip(){
     let billVal = bill.value;
     let tipVal = tip.value;
-    let totalVal = billVal * (+tipVal / 100);
+    let totalVal = billVal * (1+tipVal / 100);
     result.innerHTML = `Total Bill : ${totalVal}`;
 }
 
